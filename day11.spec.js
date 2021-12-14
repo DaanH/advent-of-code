8122be6c9fa5ae6ba3383cb4f30bc6eef25ca950
DaanH <daanheuvingh@gmail.com>
2021-12-14 17:07:46 +0100

    yeehaw!

--- day11.spec.js
+++ day11.spec.js
@@ -15,6 +15,7 @@ describe('day 11', () => {
     flashes.forEach((i) => {
       const x = i % width;
       const y = (i / width) | 0;
+
       if (y !== 0) {
         if (x !== 0) nrg[i - width - 1]++;
         nrg[i - width]++;
@@ -25,19 +26,20 @@ describe('day 11', () => {
       if (y !== height - 1) {
         if (x !== 0) nrg[i + width - 1]++;
         nrg[i + width]++;
-        if (x !== width + 1) nrg[i + width + 1]++;
+        if (x !== width - 1) nrg[i + width + 1]++;
       }
     });
   }
 
-  function step(map) {
+  function step(map, doLog = false) {
     let nrg = map.nrg.map((v) => v + 1);
     const { width, height } = map;
-    let i = 0;
 
     // console.log('next step:\n', nrgToString(nrg, width));
     let newFlashes = (allFlashes = getFlashes(nrg));
     let tries = 0;
+    doLog && console.log('------ step');
+    doLog && console.log(tries, nrgToString(nrg, width));
 
     while (newFlashes.length && tries < 100) {
       flash(nrg, width, height, newFlashes);
@@ -46,12 +48,12 @@ describe('day 11', () => {
       // console.log(nrgToString(nrg, width));
       // console.log({ newFlashes, allFlashes });
       allFlashes = [...allFlashes, ...newFlashes];
+      doLog && console.log(tries, nrgToString(nrg, width));
     }
     allFlashes.forEach((i) => (nrg[i] = 0));
 
-    console.log('end of step:');
-    console.log(nrgToString(nrg, width));
     map.nrg = nrg;
+    return allFlashes.length;
   }
 
   function nrgToString(nrg, width) {
@@ -62,14 +64,65 @@ describe('day 11', () => {
       i += width;
     }
     return chunks
-      .map((nums) => nums.map((num) => (num < 10 ? ' ' : '') + num).join(''))
+      .map((nums) => nums.map((num) => (num < 10 ? '  ' : ' ') + num).join(''))
       .join('|\n');
   }
-  test('part 1 example', () => {
+  /*  test('part 1 example 1', () => {
+    let parsed = getParsed(`6594254334
+3856965822
+6375667284
+7252447257
+7468496589
+5278635756
+3287952832
+7993992245
+5957959665
+6394862637`);
+    console.log(parsed);
+    const nrg = parsed.nrg.map((v) => v + 1);
+    const flashes = getFlashes(nrg);
+
+    expect(flashes).toEqual([
+      2, 14, 45, 49, 64, 71, 72, 74, 75, 81, 84, 86, 92,
+    ]);
+
+    console.log('before', nrgToString(nrg, 10));
+    flash(nrg, 10, 10, flashes);
+    console.log('after', nrgToString(nrg, 10));
+  });*/
+
+  test('part 1 example 10', () => {
     let parsed = getParsed(exampleInput);
-    step(parsed);
-    step(parsed);
-    expect(0).toEqual(1);
+    console.log(parsed);
+    const result = new Array(10).fill(0).reduce((acc, v, i) => {
+      const flashes = step(parsed, true);
+      console.log(`step ${i}: ${flashes}`);
+      return acc + flashes;
+    }, 0);
+    console.log('1a ', nrgToString(parsed.nrg, parsed.width));
+
+    expect(result).toEqual(204);
+  });
+
+  test('part 1 example 100', () => {
+    let parsed = getParsed(exampleInput);
+    const result = new Array(100).fill(0).reduce((acc, v, i) => {
+      const flashes = step(parsed);
+      // console.log(`step ${i}: ${flashes}`);
+      return acc + flashes;
+    }, 0);
+    console.log('1b ', nrgToString(parsed.nrg, parsed.width));
+    expect(result).toEqual(1656);
+  });
+  test('part 1', () => {
+    let parsed = getParsed(input);
+    const result = new Array(100).fill(0).reduce((acc, v, i) => {
+      const flashes = step(parsed);
+      // console.log(`step ${i}: ${flashes}`);
+      return acc + flashes;
+    }, 0);
+    console.log('fo re4l', nrgToString(parsed.nrg, parsed.width));
+    expect(result).toEqual(1705);
   });
 });
 
@@ -83,3 +136,14 @@ const exampleInput = `5483143223
 6882881134
 4846848554
 5283751526`;
+
+const input = `3113284886
+2851876144
+2774664484
+6715112578
+7146272153
+6256656367
+3148666245
+3857446528
+7322422833
+8152175168`;
