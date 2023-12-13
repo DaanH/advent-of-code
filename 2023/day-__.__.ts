78c5999ba559d87ebde6c39b499ccb0408b7b3dd
DaanH <daanheuvingh@gmail.com>
2023-12-13 16:49:07 +0100

    updated template

--- 2023/day-__.__.ts
+++ 2023/day-__.__.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from 'vitest';
 import { inputDay2 } from './input';
 
-describe('day 03 - part 1', () => {
+describe('day 07 - part 1', () => {
 	const fn = (input: string): number => {
 		const lines = input.split('\n').filter(Boolean);
 		let total = 0;
@@ -10,24 +10,13 @@ describe('day 03 - part 1', () => {
 	};
 
 	it('should work', () => {
-		const result = fn(
-			`
-467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..`
-		);
+		const result = fn(``);
 		expect(result).toBe(4361);
 	});
-
+	/*
 	it('should work with input', () => {
 		const result = fn('');
 		expect(result).toBe(2239);
 	});
+*/
 });
